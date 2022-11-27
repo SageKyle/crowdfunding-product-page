c7fbb65bfd5b0eed0d9e47e756f58b9b29596651
SageKyle <ominyipaulo@gmail.com>
2022-11-27 15:50:54 +0100

    Feat: Added navbar
    - added navigation section
    - added navigation toggle for mobile
    - styled the hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,53 @@
+// styles/images
+import Close from '../assets/icons/icon-close-menu.svg';
+import Open from '../assets/icons/icon-hamburger.svg';
+import './Navbar.scss';
+
+import { useRef } from 'react';
+
 export const Navbar = () => {
+	const navRef = useRef('');
+	const openNavRef = useRef('');
+	const closeNavRef = useRef('');
+
+	const handleRef = () => {
+		navRef.current.classList.toggle('hidden');
+		openNavRef.current.classList.toggle('hidden');
+		closeNavRef.current.classList.toggle('hidden');
+	};
+
 	return (
 		<header className="navbar">
 			<h1 className="navbar__brand">crowdfund</h1>
-			<nav className="navbar__nav"></nav>
+			<nav className="navbar__container">
+				<div className="navbar__toggle">
+					<img
+						src={Open}
+						ref={openNavRef}
+						onClick={handleRef}
+						alt="Open Navigation"
+						className="logo navbar__toggle--open"
+					/>
+					<img
+						src={Close}
+						ref={closeNavRef}
+						onClick={handleRef}
+						alt="Close Navigation"
+						className="logo hidden navbar__toggle--close"
+					/>
+				</div>
+				<section className="navbar__nav hidden" ref={navRef}>
+					<a href="/" className="navbar__nav--link">
+						about
+					</a>
+					<a href="/" className="navbar__nav--link">
+						discover
+					</a>
+					<a href="/" className="navbar__nav--link">
+						get started
+					</a>
+				</section>
+			</nav>
 		</header>
 	);
 };
--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,13 +1,12 @@
 // styles
 import './Hero.scss';
 
-import { Navbar } from './components/Navbar';
+import { Navbar } from '../../components/Navbar';
 
 export const Hero = () => {
 	return (
 		<section className="hero">
 			<Navbar />
-			<div className="hero-item">This is the hero container</div>
 		</section>
 	);
 };
