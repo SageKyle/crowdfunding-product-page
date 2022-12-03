784b83cab94901aa8d55341af5e169af3f25228e
SageKyle <ominyipaulo@gmail.com>
2022-12-03 01:49:22 +0100

    fixed the stats display component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { PledgeModal } from './modals/PledgeModal';
 import { CTA } from './sections/cta/CTA';
 import { Hero } from './sections/hero/Hero';
 import { Project } from './sections/project/Project';
+import { Stats } from './sections/stats/Stats';
 
 import { useState } from 'react';
 
@@ -15,6 +16,7 @@ function App() {
 		<div className="App">
 			<Hero />
 			<CTA setShowModal={setShowModal} />
+			<Stats />
 			<Project />
 			{showModal && <PledgeModal setShowModal={setShowModal} number={number} />}
 		</div>
--- src/sections/stats/Stats.jsx
+++ src/sections/stats/Stats.jsx
@@ -4,7 +4,21 @@ import './Stats.scss';
 export const Stats = () => {
 	return (
 		<section className="stats">
-			<h4>This is the stats container</h4>
+			<div className="stats__section">
+				<h4 className="stats__section--heading">$89,914</h4>
+				<p className="stats__section--info">of $100,000 backed</p>
+			</div>
+			<div className="stats__section">
+				<h4 className="stats__section--heading">5,007</h4>
+				<p className="stats__section--info">total backers</p>
+			</div>
+			<div className="stats__section">
+				<h4 className="stats__section--heading">56</h4>
+				<p className="stats__section--info">days left</p>
+			</div>
+			<div className="stats__progress--backdrop">
+				<div className="stats__progress--foreground"></div>
+			</div>
 		</section>
 	);
 };
